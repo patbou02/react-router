6d2a64dc4bc7d1c51c4f3a4321c14c92d7b3961a
Patrick B <>
2023-01-08 18:49:20 -0800

    Added dummy quotes and passed them to the QuoteList component. In the NewQuote page, connected the QuoteForm component and passed it a function which logs a quote entered in the form.

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,13 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'p1', author: 'Pat', text: 'My first quote', },
+  { id: 'p2', author: 'Patrick', text: 'My second quote', },
+];
+
 const AllQuotes = () => {
   return (
-    <h1>All Quotes Page</h1>
+    <QuoteList quotes={DUMMY_QUOTES} />
   );
 };
 
